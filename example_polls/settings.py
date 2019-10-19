96e220c69b9e8a1335635f7df0969ea0ab2e0800
anathik <me@anathi.io>
2019-10-19 19:37:15 -0400

    polls app and docker compose

--- example_polls/settings.py
+++ example_polls/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'example_polls.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'polls_db',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '192.168.1.219',
+        'PORT': '54320'
     }
 }
 
